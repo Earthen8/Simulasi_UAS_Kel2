99728f7fe8cbfd7413b800aa10a403ed1f74de00
unknown <earthen505@gmail.com>
2025-11-16 15:07:38 +0700

    lol

--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react'; 
 import { useParams, useNavigate } from 'react-router-dom';
 import api from '../services/api';
-import AuthContext from '../context/AuthContext';
+import { AuthContext } from '../context/AuthContext';
 import { Package, Plus, Minus, CheckCircle, XCircle } from 'lucide-react';
 import { toast } from 'react-hot-toast';
 
