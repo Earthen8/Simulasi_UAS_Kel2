528e67fb6873fb720c31a7ec4279ae671aad2e1b
unknown <earthen505@gmail.com>
2025-11-16 13:53:49 +0700

    gubffgg

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { Toaster } from 'react-hot-toast';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
